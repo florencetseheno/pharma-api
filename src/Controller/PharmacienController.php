315ca4a71312192de6fe9459a22e367953dfa972
Andryfrederic <andryfrederic2@gmail.com>
2021-03-01 19:33:54 +0300

    commencement/ controlleur get all pharmacien

---
+++ src/Controller/PharmacienController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+use App\Repository\PharmacienRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class PharmacienController extends AbstractController
+{
+    /**
+     * @Route("/api/pharmacien", name="pharmacien", methods={"GET"})
+     */
+    public function index(PharmacienRepository $PharmacienRepository,NormalizerInterface $normalizer  )
+    {
+        $pharmacien = $PharmacienRepository->findAll();
+        $pharmacienNormalises = $normalizer->normalize($pharmacien);
+        // dd( $pharmacienNormalises );
+        $json=json_encode($pharmacienNormalises);
+        dd($json);
+
+        
+        return $this->render('pharmacien/index.html.twig', [
+            'controller_name' => 'PharmacienController',
+        ]);
+    }
+}
---
+++ src/Entity/Pharmacien.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\PharmacienRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=PharmacienRepository::class)
+ */
+class Pharmacien
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nom_pharmacien;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $prenom_pharmacien;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomPharmacien(): ?string
+    {
+        return $this->nom_pharmacien;
+    }
+
+    public function setNomPharmacien(string $nom_pharmacien): self
+    {
+        $this->nom_pharmacien = $nom_pharmacien;
+
+        return $this;
+    }
+
+    public function getPrenomPharmacien(): ?string
+    {
+        return $this->prenom_pharmacien;
+    }
+
+    public function setPrenomPharmacien(string $prenom_pharmacien): self
+    {
+        $this->prenom_pharmacien = $prenom_pharmacien;
+
+        return $this;
+    }
+}
